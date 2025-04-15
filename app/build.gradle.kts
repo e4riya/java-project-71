2db6ffc1c65074e9bb7f53eafcba7b258cc08a65
e4riya <kotkovdima0@gmail.com>
2025-04-16 02:49:08 +0300

    app not sumodulegit status!

---
+++ app/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id ("com.github.ben-manes.versions") version "0.38.0"
+    application
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+application {
+    mainClass = "hexlet.code.App"
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("info.picocli:picocli:4.7.6")
+}
+
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,28 @@
+package hexlet.code;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Option;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+                     description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
+    @Parameters(index = "0", description = "path to first file", paramLabel = "filepath1")
+    private String firstFilePath;
+    @Parameters(index = "1", description = "path to second file", paramLabel = "filepath2")
+    private String secondFilepath;
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+    @Option (names = {"-f", "--format"}, description = "output format ${DEFAULT-VALUE}",
+             defaultValue = "stylish",paramLabel = "format")
+    private String format;
+    @Override
+    public Integer call(){
+        return 0;
+    }
+}
